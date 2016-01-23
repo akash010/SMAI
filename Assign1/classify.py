bcb110de2b1ec85f90a080f392c1d5290ff6fbba
Akash <akash.iiithyd@gmail.com>
2016-01-23 16:13:46 +0530

    Added code for graphs

--- Assign1/classify.py
+++ Assign1/classify.py
@@ -4,6 +4,7 @@ import random
 import math
 import numpy
 import matplotlib.pyplot as plt
+import operator
 
 def randomSubSampling(dataset):
 	Testdataset = []
@@ -224,7 +225,106 @@ def calculate3NNfold(dataset, classes, classColumnNum):
 	print "Mean: " + str(arr.mean())
 	print "Standard Deviation: " + str(arr.std())
 	print "**************************\n"
-	
+
+def decisonLine(dataPoints, plt):	
+
+	ln1=[]
+	ln2=[]
+	ln3=[]
+	ln4=[]
+
+	del dataPoints[-1]
+	dataPoints = sorted(dataPoints, key = operator.itemgetter(3))
+	dataPoints = sorted(dataPoints, key = operator.itemgetter(1))
+
+	point1={}
+	point2={}
+
+	ind=0
+	length=len(dataPoints)
+
+	for var in range(45):
+		x=float(var)/10
+
+		if ind >= length:
+			break
+
+		temp=""
+
+		while dataPoints[ind][1]==str(x):
+			if temp!="" and temp!=dataPoints[ind][4]:
+				if temp=='Iris-setosa':
+					point1[x]=float(dataPoints[ind-1][3])
+				elif temp=='Iris-versicolor':
+					point2[x]=float(dataPoints[ind-1][3])
+
+			temp=dataPoints[ind][4]			
+			ind+=1
+			if ind >= length:
+				break
+
+		if temp=='Iris-setosa':
+			point1[x]=float(dataPoints[ind-1][3])
+		elif temp=='Iris-versicolor':
+			point2[x]=float(dataPoints[ind-1][3])
+
+	point1[0]=0.25
+	point2[0]=1
+	point1[4.5]=0.5
+	point2[4.5]=1.5
+
+	for var in range(46):
+		x=float(var)/10
+		if x in point1:
+			ln1.append(x)
+			ln2.append(point1[x])
+
+		if x in point2:
+			ln3.append(x)
+			ln4.append(point2[x])	
+
+	plt.plot(ln1,ln2,'r')
+	plt.plot(ln3,ln4,'g')
+	plt.legend(loc='upper left')
+	plt.axis([0, 4.3, 0, 2.8])
+	plt.xlabel("widthSepal")
+	plt.ylabel("widthPetal")
+	plt.show()	
+
+def plotGraph(dataPoints):
+	widthSep = []
+	temp = []
+	for i in range(3):
+		for j in range(50):
+			temp.append(0)
+		widthSep.append(temp)
+		temp = []
+
+	widthPet = []
+	temp = []
+	for i in range(3):
+		for j in range(50):
+			temp.append(0)
+		widthPet.append(temp)
+		temp = []
+
+	listClasses=['Iris-setosa','Iris-versicolor','Iris-virginica']
+
+	for i in range(3):
+		j=0
+		for k in range(len(dataPoints) - 1):
+			temp=dataPoints[k]
+			if temp[4]==listClasses[i]:
+				widthPet[i][j]=temp[3]
+				widthSep[i][j]=temp[1]
+				j= j + 1
+
+	plt.plot(widthSep[0],widthPet[0], 'ro',label='Iris-setosa')
+	plt.plot(widthSep[1],widthPet[1], 'go', label='Iris-versicolor')
+	plt.plot(widthSep[2],widthPet[2], 'bo',label='Iris-virginica')
+
+	decisonLine(dataPoints, plt)
+
 def main():
 	fileName, classColumnNum = getInputArgs()
 	dataset = getDataset(fileName,classColumnNum)
@@ -237,20 +337,13 @@ def main():
 	calculate1NNfold(dataset, classes, classColumnNum)
 	calculate3NNfold(dataset, classes, classColumnNum)
 
-	x = []
-	y = []
+	del dataset
 
-	for i in range(len(dataset)):
-		for j in range(len(dataset[i])):
-			if j ==1:
-				x.append(dataset[i][j])
-			if j == 3:
-				y.append(dataset[i][j])
+	with open(fileName, 'rb') as f:
+		reader = csv.reader(f)
+		dataset = list(reader)
+		plotGraph(dataset)	
 
-	plt.plot(x, y, 'ro')
-	plt.show()
 	del dataset
 
 main()
-
-
